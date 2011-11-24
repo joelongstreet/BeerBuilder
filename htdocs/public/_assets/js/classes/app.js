53e9c13b1b2439c47a1ce3e9a1ab7eeaca58f3b6
Joe Longstreet <joelongstreet@gmail.com>
2011-11-23 21:54:16 -0600

    menu now supports touch gestures

--- htdocs/public/_assets/js/classes/app.js
+++ htdocs/public/_assets/js/classes/app.js
@@ -4,10 +4,40 @@ var app = function(){
 		var $nav		= $('nav#main');
 		var $shadow = $('div#nav_shadow');
 		var $handle = $nav.find('.handle');
+		var $nav_w	= parseInt($nav.css('width'));
+
+		var touch_start	= 0;
+		var touch_percent	= 0;
+		var is_open 		= 0;
 		
-		$handle.bind('click', function(){
-			$nav.toggleClass('open');
-			$shadow.toggleClass('on');
+		$handle.bind('touchstart', function(e){
+			touch_start = e.pageX;
+		});
+		
+		$handle.bind('touchmove', function(e){
+			if(is_open == 0){
+				touch_percent = -1*(100 - (((e.pageX - touch_start)/$nav_w)*100));
+			} else{
+				touch_percent = -1*(((touch_start - e.pageX)/$nav_w)*100);
+			}
+
+			if(touch_percent < 0 && touch_percent > -97){
+				var translate_string = 'translateX(' + touch_percent + '%' + ')';
+				$nav.css({ '-webkit-transform' : translate_string });	
+			}
+		});
+		
+		$handle.bind('touchend', function(e){
+			$nav.addClass('animate');
+			if(touch_percent > -50) {
+				setTimeout(function(){ $nav.css({ '-webkit-transform' : 'translateX(0%)' }); });
+				is_open = 1;
+			} else {
+				setTimeout(function(){ $nav.css({ '-webkit-transform' : 'translateX(-97%)' }); });
+				is_open = 0;
+			}
+			
+			setTimeout(function(){ $nav.removeClass('animate'); }, 300);
 		});
 	};
 	
