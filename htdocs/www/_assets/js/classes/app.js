2e64a42ada1fe8bb438d644cff75ac00de519580
Joe Longstreet <joelongstreet@gmail.com>
2011-11-25 09:43:34 -0600

    now has to be compatible for xcode and ios deployment. changing public to www.

--- htdocs/www/_assets/js/classes/app.js
+++ htdocs/www/_assets/js/classes/app.js
@@ -61,6 +61,15 @@ var app = function(){
 	//TODO build object should be in here. or I should have a way to call it.
 	//how am I planning on switching between application functions? eg builder, my recipes, etc
 	window.builder = new Builder();
+	
+	var $body = $('body');
+	if($.os.ios){
+		$body.addClass('ios');
+	} else if($.os.android){
+		$body.addClass('android');
+	}	else {
+		$body.addClass('desktop');
+	}
 };
 
 $(function(){ var my_app = new app(); });
